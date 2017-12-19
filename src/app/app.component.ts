c8dfa764736033f11c8264574b79dc7538032914
ObaidurRehman <obaid.urrehman@gmail.com>
2017-12-20 03:16:35 +0500

    change detection optimization with immutable js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { City } from './city';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
+import { List } from 'immutable';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,13 @@ import 'rxjs/add/operator/delay';
 export class AppComponent {
   title = 'app';
 
-  cities = [new City('Karachi'), new City('Lahore'), new City('Islamabad')];
+  cities = List<City>([new City('Karachi'), new City('Lahore'), new City('Islamabad')]);
   city: string;
 
   addCity(event: any) {
     if (event.key === 'Enter' && this.city !== undefined) {
-      this.cities.push(new City(this.city));
-      // this.cities = this.stupidClone(this.cities);
+      this.cities = this.cities.push(new City(this.city));
       this.city = '';
     }
-  }
-
-  stupidClone (src) {
-    return JSON.parse(JSON.stringify(src));
   }
 }
